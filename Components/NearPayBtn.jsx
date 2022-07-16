93391f316a2da01fc7c12cbc3cda875bacac0943
Sandeep <itissandeep98@gmail.com>
2022-07-17 00:33:10 +0530

    working without private key

--- Components/NearPayBtn.jsx
+++ Components/NearPayBtn.jsx
@@ -1,59 +1,17 @@
-import React from "react";
+import { utils } from "near-api-js";
 import Image from "next/image";
 import { showAlert } from "./Alert";
-import {
-	utils,
-	providers,
-	transactions,
-	KeyPair,
-	keyStores,
-	connect,
-} from "near-api-js";
-import { sha256 } from "js-sha256";
 
-const keyData = utils.KeyPairEd25519.fromString(
-	"ed25519:PtzQMbaQvKM5236qufnBLEJhG2rxU1sXJffMVF6x4gdxABFqGNtQcrNdUyUFTY2fFZrj1PCJKrBMuNvByo4M2xd"
-);
-const provider = new providers.JsonRpcProvider(`https://rpc.testnet.near.org`);
-
-const getUserData = async () => {
-	return await provider.query(
-		`access_key/test-sandy.testnet/${keyData.getPublicKey().toString()}`,
-		""
-	);
-};
-
-function NearPayBtn({ value, account_id }) {
+function NearPayBtn({ value, near, account_id }) {
 	const sendTokens = async () => {
 		try {
-			const accessKey = await getUserData();
-			const nonce = ++accessKey.nonce;
-			const amount = utils.format.parseNearAmount("1");
-			const blockHash = utils.serialize.base_decode(accessKey.block_hash); // base58 to buffer
-			const unsignedTransaction = transactions.createTransaction(
-				account_id,
-				keyData.getPublicKey(),
+			const senderAccount = await near.account(account_id);
+			const amount = utils.format.parseNearAmount(value);
+			showAlert("Sending tokens...");
+			const result = await senderAccount.sendMoney(
 				"itissandeep98.testnet",
-				nonce,
-				[transactions.transfer(amount)],
-				blockHash
+				amount
 			);
-			console.log(unsignedTransaction.encode().toString("base64"));
-			const serializedTxHash = new Uint8Array(
-				sha256.array(unsignedTransaction.encode())
-			);
-			const signedTxHash = keyData.sign(serializedTxHash);
-			const signedTransaction = new transactions.SignedTransaction({
-				transaction: unsignedTransaction,
-				signature: new transactions.Signature({
-					keyType: unsignedTransaction.publicKey.keyType, // ED25519 = 0; // only this is supported in js sdk for now
-					data: signedTxHash.signature,
-				}),
-			});
-			const signedSerializedTx = signedTransaction.encode();
-			const result = await provider.sendJsonRpc("broadcast_tx_commit", [
-				Buffer.from(signedSerializedTx).toString("base64"),
-			]);
 			showAlert("Transaction sent successfully", "success");
 			console.log(result);
 		} catch (error) {
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,45 @@
+import { KeyPair } from "near-api-js";
 import { useRouter } from "next/router";
+
+const PENDING_ACCESS_KEY_PREFIX = "pending_key";
 
 function Index({ wallet }) {
 	const router = useRouter();
+	console.log(wallet);
 
 	if (wallet?.isSignedIn()) {
 		router.push("/home");
 	}
 
-	const signIn = () => {
-		wallet.requestSignIn(
-			"example-contract.testnet", // contract requesting access
-			"Mazout", // optional
-			`${window.location}home` // optional
+	const loginFullAccess = async (options) => {
+		const currentUrl = new URL(window.location.href);
+		const newUrl = new URL(wallet._walletBaseUrl + "/login/");
+		newUrl.searchParams.set(
+			"success_url",
+			options.successUrl || currentUrl.href
 		);
+		newUrl.searchParams.set(
+			"failure_url",
+			options.failureUrl || currentUrl.href
+		);
+
+		const accessKey = KeyPair.fromRandom("ed25519");
+		newUrl.searchParams.set("public_key", accessKey.getPublicKey().toString());
+		await wallet._keyStore.setKey(
+			wallet._networkId,
+			PENDING_ACCESS_KEY_PREFIX + accessKey.getPublicKey(),
+			accessKey
+		);
+
+		window.location.assign(newUrl.toString());
 	};
 
 	return (
 		<div className="border p-20 rounded-xl w-full">
 			<button
-				onClick={signIn}
+				onClick={() =>
+					loginFullAccess({ successUrl: `${window.location}home` })
+				}
 				className="text-white block m-4  focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 bg-cyan-600 hover:bg-cyan-700 focus:ring-cyan-900"
 			>
 				Sign In
