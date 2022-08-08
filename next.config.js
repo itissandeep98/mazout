71b5ea3d9bb1c6b85302067ea26291cd925ab83b
Sandeep Kumar Singh <itissandeep98@gmail.com>
2022-08-08 09:40:37 +0530

    Update pages.yml

--- next.config.js
+++ next.config.js
@@ -1,22 +1,25 @@
 // This file was automatically added by layer0 init.
 // You should commit this file to source control.
-const { withLayer0, withServiceWorker } = require('@layer0/next/config')
+const { withLayer0, withServiceWorker } = require("@layer0/next/config");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-const _preLayer0Export = nextConfig;;
+	reactStrictMode: true,
+	swcMinify: true,
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
+	},
+};
 
 module.exports = (phase, config) =>
-  withLayer0(
-    withServiceWorker({
-      // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-      // the logs in the Layer0 developer console.
-      layer0SourceMaps: true,
+	withLayer0(
+		withServiceWorker({
+			// Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
+			// the logs in the Layer0 developer console.
+			layer0SourceMaps: true,
 
-      ..._preLayer0Export
-    })
-  )
+			...nextConfig,
+		})
+	);
